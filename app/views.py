d46d290e607c564ae3d79d6834aec6070786e19d
Daniel Spiers <danjspiers@gmail.com>
2024-11-27 17:22:44 +0000

    user page frontend started

--- app/views.py
+++ app/views.py
@@ -18,12 +18,14 @@ def home():
     # if logged in show all posts in a feed ordered newestFirst/mostLiked/etc...
     return render_template("home.html", title="Home")
 
-@app.route("/user/<int:userId>", methods=['GET', 'POST'])
-def profile(userId=None):
+@app.route("/user/<string:username>", methods=['GET', 'POST'])
+def profile(username=None):
     # Show a user's profile page if logged in
     # redirect to login page if logged out
-        
-    return render_template("base.html", title="User Profile Page")
+    
+
+
+    return render_template("userProfilePage.html", title=f"{username}")
 
 @app.route("/create-post", methods=['GET', 'POST'])
 def createPost():
@@ -44,12 +46,14 @@ def createPost():
         db.session.add(new_post)
         db.session.commit()
 
-        return redirect(f"/user/{post_author}")
+        author = db.session.execute(db.select(User).filter_by(id=post_author)).scalar()
+
+        return redirect(f"/user/{author.username}")
 
     return render_template("createPost.html", title="Create a Post", form=form)
 
-@app.route("/view-post/<int:postId>", methods=['GET', 'POST'])
-def viewPost(postId=None):
+@app.route("/view-post/<int:post_id>", methods=['GET', 'POST'])
+def viewPost(post_id=None):
     # view a specific post from a user in order to comment on it
     # redirect to login if logged out
     pass
