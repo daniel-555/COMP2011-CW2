7da54ed84c8baf6abae2f17ef191e1262b63d392
Daniel Spiers <danjspiers@gmail.com>
2024-12-03 23:58:11 +0000

    cleaned up imports

--- app/auth.py
+++ app/auth.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect
-from flask_login import current_user, login_required, login_user, logout_user
+from flask_login import current_user, login_user, logout_user
 from app import app, db, login_manager
 from .models import User
 from .forms import LoginForm, RegisterForm
